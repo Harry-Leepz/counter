88736d106955ee0092f3eea5cda6e77ca88abf26
Harry Dhillon <hdhillon478@gmail.com>
2024-06-01 18:11:31 +0100

    Add logic to increment count using spacebar press

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Count from "./Count";
 import ButtonContainer from "./ButtonContainer";
 import ResetButton from "./ResetButton";
@@ -7,6 +7,27 @@ import Title from "./Title";
 export default function Card() {
   const [count, setCount] = useState(0);
   const locked = count === 5 ? true : false;
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      // event listener for spacebar keydown
+      if (e.code === "Space") {
+        const newCount = count + 1;
+        // Ensure count never goes above 5
+        if (newCount > 5) {
+          setCount(5);
+          return;
+        }
+        setCount(newCount);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      // Remove event listener when component unmounts and prevent app from crashing due to excessive event listeners being added
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [count]);
 
   return (
     <div className={`card ${locked && "card--limit"}`}>
